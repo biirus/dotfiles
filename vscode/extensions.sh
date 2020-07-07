1bd8905bd1ae2a6f731c925e8c2f9c49ceb8db97
Ivan Burnaev <ivan.biirus@gmail.com>
2020-07-07 19:02:43 +0300

    Add extensions list

---
+++ vscode/extensions.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+code --install-extension arcticicestudio.nord-visual-studio-code
+code --install-extension ardenivanov.svelte-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension elmTooling.elm-ls-vscode
+code --install-extension esbenp.prettier-vscode
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension JamesBirtles.svelte-vscode
+code --install-extension jspolancor.presentationmode
+code --install-extension karigari.chat
+code --install-extension miguelsolorio.min-theme
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension ms-vsliveshare.vsliveshare-audio
+code --install-extension ms-vsliveshare.vsliveshare-pack
+code --install-extension RobbOwen.synthwave-vscode
+code --install-extension teabyii.ayu
