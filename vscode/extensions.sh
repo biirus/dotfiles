071c09e4827c849c3e4dd030d0260144de767378
Ivan Burnaev <ivan.biirus@gmail.com>
2020-07-27 11:30:59 +0300

    Update VSCode extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -3,6 +3,7 @@
 code --install-extension arcticicestudio.nord-visual-studio-code
 code --install-extension ardenivanov.svelte-intellisense
 code --install-extension dbaeumer.vscode-eslint
+code --install-extension donjayamanne.githistory
 code --install-extension eamodio.gitlens
 code --install-extension EditorConfig.EditorConfig
 code --install-extension elmTooling.elm-ls-vscode
@@ -11,10 +12,14 @@ code --install-extension Gruntfuggly.todo-tree
 code --install-extension JamesBirtles.svelte-vscode
 code --install-extension jspolancor.presentationmode
 code --install-extension karigari.chat
+code --install-extension kube.theme-kay
 code --install-extension miguelsolorio.min-theme
+code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 code --install-extension ms-vsliveshare.vsliveshare
 code --install-extension ms-vsliveshare.vsliveshare-audio
 code --install-extension ms-vsliveshare.vsliveshare-pack
 code --install-extension octref.vetur
+code --install-extension RobbOwen.synthwave-vscode
 code --install-extension teabyii.ayu
+code --install-extension vscodevim.vim
